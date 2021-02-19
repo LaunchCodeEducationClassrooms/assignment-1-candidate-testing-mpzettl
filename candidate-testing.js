a2ee054d5a4f603f1038f6a4d5b3462d29da0863
Peter Zettl <mpzettl@gmail.com>
2021-02-19 17:29:39 +0000

    trailing spaces added

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,10 +5,10 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ["Who was the first American woman in space? ", "True or False: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let question = ["Who was the first American woman in space? ", "True or False: 5000 meters = 5 kilometers. ","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswer = ["Sally Ride", "True","40", "Trajectory", "3"];
 let candidateAnswer = "";
-let questions =["Who was the first American woman in space? ", "True or False: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let questions =["Who was the first American woman in space? ", "True or False: 5000 meters = 5 kilometers. ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers=["Sally Ride", "True","40", "Trajectory", "3"];
 let candidateAnswers = [];
 
